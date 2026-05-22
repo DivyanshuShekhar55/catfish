6c6b97038d21165481543551b20e9b26280371e3
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-05-22 11:47:53 +0530

    BUG : fixed back = 0 with non New() initialisations

--- utils/deque/deque.go
+++ utils/deque/deque.go
@@ -20,9 +20,10 @@ const (
 // This makes push/pop at both ends super cheap — no copying needed.
 
 type Deque[T any] struct {
-	a     []T // the actual storage
-	front int // index of the first item
-	back  int // index of the last item (-1 means empty)
+	a           []T  // the actual storage
+	front       int  // index of the first item
+	back        int  // index of the last item (-1 means empty)
+	initialised bool // Explicitly tracks if back needs to start at -1, look below for explanation
 }
 
 func New[T any](capacity int) *Deque[T] {
@@ -30,8 +31,29 @@ func New[T any](capacity int) *Deque[T] {
 		panic("catfish/deque : capacity must be greater than 0")
 	}
 	return &Deque[T]{
-		a:    make([]T, capacity),
-		back: -1,
+		a:           make([]T, capacity),
+		back:        -1,
+		initialised: true, // those who don't run New() have this set as false
+	}
+}
+
+// an important edge case to consider :
+// go allows users to initialise structs as var s Deque[int]
+// since it doesn't call the New() it inits a nil slice
+// this can cause panic when checking for d.a[any_index] or when pushing or popping items to the slice
+// so we apply a d.a == nil check in all the functionalities
+// another issue is this type of init sets front = back = 0 (front is fine, but not back)
+// this cause problem firstly as checks where d.back == -1 would be used if it was New() initialised
+// so we replace all d.back == -1 checks with d.Len() == 0
+// secondly also create a init-checker function that initialises correctly by checking the initialised flag
+/// if a user bypasses New(), it catches d.back being 0 and forces it to -1
+
+// ensureInit safely configures the zero-value struct on the fly
+func (d *Deque[T]) ensureInit() {
+	if !d.initialised {
+		d.a = make([]T, CAPACITY)
+		d.back = -1
+		d.initialised = true
 	}
 }
 
@@ -47,12 +69,6 @@ func (d *Deque[T]) Len() int {
 	return len(d.a) - d.front + d.back + 1
 }
 
-// an important edge case to consider : 
-// go allows users to initialise structs as var s Deque[int]
-// since it doesn't call the New() it inits a nil slice
-// this can cause panic when checking for d.a[any_index] or when pushing or popping items to the slice
-// so we apply a d.a == nil check in all the functionalities 
-
 func (d *Deque[T]) IsFull() bool {
 	if d.a == nil {
 		return false
@@ -67,6 +83,9 @@ func (d *Deque[T]) IsFull() bool {
 }
 
 func (d *Deque[T]) PushFront(x T) error {
+
+	d.ensureInit()
+
 	// Lazy allocate memory if struct was initialized as a zero-value
 	if d.a == nil {
 		d.a = make([]T, CAPACITY)
@@ -80,7 +99,7 @@ func (d *Deque[T]) PushFront(x T) error {
 	d.a[d.front] = x
 
 	// if deque was earlier empty, update the back pointer
-	if d.back == -1 {
+	if d.Len() == 0 {
 		d.back = d.front
 	}
 
@@ -89,6 +108,9 @@ func (d *Deque[T]) PushFront(x T) error {
 }
 
 func (d *Deque[T]) PushBack(x T) error {
+
+	d.ensureInit()
+
 	// Lazy allocate memory if struct was initialized as a zero-value
 	if d.a == nil {
 		d.a = make([]T, CAPACITY)
@@ -97,7 +119,7 @@ func (d *Deque[T]) PushBack(x T) error {
 	if d.IsFull() {
 		return errDequeFull
 	}
-	if d.back == -1 {
+	if d.Len() == 0 {
 		d.back = d.front
 	} else {
 		d.back = (d.back + 1) % len(d.a)
@@ -149,7 +171,7 @@ func (d *Deque[T]) PopBack() T {
 
 // Front returns the item at the front of the deque. It panics if the deque is empty.
 func (d *Deque[T]) Front() T {
-	if d.back == -1 {
+	if d.Len() == 0 {
 		panic("catfish/deque: index out of range")
 	}
 	return d.a[d.front]
@@ -158,7 +180,7 @@ func (d *Deque[T]) Front() T {
 // Back returns the item at the back of the deque. It panics if the deque is empty.
 func (d *Deque[T]) Back() T {
 	// Added empty check protection to prevent runtime d.a[-1] crash
-	if d.back == -1 {
+	if d.Len() == 0 {
 		panic("catfish/deque: index out of range")
 	}
 
