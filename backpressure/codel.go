d483c34835a0953fba5cc9fbf79669c773248a4e
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-05-22 18:36:15 +0530

    codel waiter core functions implemented with comments

--- backpressure/codel.go
+++ backpressure/codel.go
@@ -1,6 +1,9 @@
 package backpressure
 
 import (
+	"context"
+	"errors"
+	"sync/atomic"
 	"time"
 
 	"github.com/DivyanshuShekhar55/catfish/utils/deque"
@@ -18,24 +21,37 @@ type coDel[T any] struct {
 	shortTimeout time.Duration
 	longTimeout  time.Duration
 	lastEmptied  time.Time
-	mode         int
+	mode         coDelMode
 	items        *deque.Deque[*coDelWaiter[T]]
 }
 
+type coDelMode int
+
+const (
+	coDelModeFIFO coDelMode = iota
+	coDelModeLIFO
+)
+
+var ErrRejected error = errors.New("catfish/backpressure : request rejected, too many requests")
+
 // true if there are no waiters in the codel.
 func (c *coDel[T]) empty() bool {
-	//return c.items.Len() == 0
-	return true
+	return c.items.Len() == 0
 }
 
 // Once a request is queued it is wrapped around in a codelWaiter struct. Why ?
 // Because all requets have this context attached to a timeout (say 5 seconds), in highly concurrent systems it may happen
-// that a request is accepted and it context timed-out at the same time - should we discard it or accept+execute it?
-// there is a contention here and the only guarantee must be "only one operation is performed on such requests"
-// there is a bool chan used with each request - context's cancel() should try to send a "false" to this chan
-// accept() should try to send a "true"
-// the winner takes the rights of the request
-// once the winner is decided, it also updated the 'state' of the request
+// that a request is accepted and it context timed-out (<-ctx.Done()) at the same time - should we discard it or accept+execute it?
+// there is a contention here and the only guarantee must be "only one of these operations is performed on such requests"
+// there is a atomic state used with each request - context's close should try to make it = 2
+// accept() should try to make it = 1
+// the winner of the atomic swap operation takes the rights of the request
+// so if state=1 wins, it is accepted, else rejected.
+
+// Note that there is no contention between context timeout and drop() because both reject the request, no harm if both run
+// the drop function is called by the reap()er function
+// the admit() and drop() put the reject or accept signal into the 'c' channel
+// accept() sends a true while drop() sends a false to this boolean channel
 
 type coDelWaiter[T any] struct {
 	// time of enqueue, used for reap
@@ -65,4 +81,52 @@ func NewCoDel[T any](shortTimeout, longTimeout time.Duration) *coDel[T] {
 		mode:         0,
 		items:        deque.New[*coDelWaiter[T]](32),
 	}
+}
+
+// next we want to implement wait, admit and drop functions
+// wait will keep the request waiting until context timeouts or admit or the reap "wakes" it up
+// which means these action(s) try to take the rights of that request and change its 'state'
+// the context is passed all the way from request to codel to here
+func (cw *coDelWaiter[T]) wait(ctx context.Context) error {
+
+	// the select is responsible for keeping the request blocked, smthing must happen (the cases) to wake it up
+	select {
+	// case 1: context timed out / cancelled
+	case <-ctx.Done():
+		// context cancel
+		// try to update the state atomically
+		// if state was = 0 (waiting), set to 2 (client expired) now
+		didSwap := atomic.CompareAndSwapUint32(&cw.state, 0, 2)
+		if !didSwap {
+			// failed to swap, state value must already have been = 1
+			// i.e., admitted => no error
+			return nil
+		}
+		// ctx cancelled succesfully, return error = context error to let user know
+		return ctx.Err()
+
+	case v := <-cw.c:
+		if v {
+			// v is true, request accepted by codel
+			return nil
+		} else {
+			// reap rejected the request, send error
+			return ErrRejected
+		}
+	}
+
+}
+
+// admit will attempt to admit the request, returns true if admission successfull
+func (wc *coDelWaiter[T]) admit() bool {
+	isAdmitted := atomic.CompareAndSwapUint32(&wc.state, 0, 1)
+	wc.c <- true
+	return isAdmitted
+}
+
+// drop unsuccessfully-wakes the waiter, it will return ErrRejected from wait.
+// we don't need any state swaps as there is no contention between drop with admit or with context timeout
+// simply send a false
+func (w *coDelWaiter[T]) drop() {
+	w.c <- false
 }
